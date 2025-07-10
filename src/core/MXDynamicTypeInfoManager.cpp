d728e9f33e19b00c52db949e193d15b5e3c18d1c
Mux <muxxum65536@gmail.com>
2025-07-10 22:15:24 +0900

    bugfix

--- src/core/MXDynamicTypeInfoManager.cpp
+++ src/core/MXDynamicTypeInfoManager.cpp
@@ -4,34 +4,34 @@
 #include "mxspp/core/MXDynamicTypeTable.h"
 
 namespace mxs::core {
-     auto MXDynamicTypeInfoManager::get_manager() -> MXDynamicTypeInfoManager & {
-         static MXDynamicTypeInfoManager manager;
-         return manager;
-     }
+    //  auto MXDynamicTypeInfoManager::get_manager() -> MXDynamicTypeInfoManager & {
+    //      static MXDynamicTypeInfoManager manager;
+    //      return manager;
+    //  }
 
     auto MXDynamicTypeInfoManager::get_rtti() -> MXRuntimeTypeInfo & {
-         static MXRuntimeTypeInfo rtti {
-             "mxs::core::MXDynamicTypeInfoManager", &MXObject::get_rtti(),
-         };
+        static MXRuntimeTypeInfo rtti{
+            "mxs::core::MXDynamicTypeInfoManager",
+            &MXObject::get_rtti(),
+        };
 
-         return rtti;
-     }
-    auto MXDynamicTypeInfoManager::register_newtype(const MXRuntimeTypeInfo *const obj) -> void {
-         std::scoped_lock guard(this->lock);
-         this->type_infos.insert(obj);
-     }
+        return rtti;
+    }
+    auto MXDynamicTypeInfoManager::register_newtype(const MXRuntimeTypeInfo *const obj)
+            -> void {
+        std::scoped_lock guard(this->lock);
+        this->type_infos.insert(obj);
+    }
 
-    auto MXDynamicTypeInfoManager::unregister_type(const MXRuntimeTypeInfo *const obj) -> void {
-         std::scoped_lock guard(this->lock);
-         this->type_infos.erase(obj);
-     }
+    auto MXDynamicTypeInfoManager::unregister_type(const MXRuntimeTypeInfo *const obj)
+            -> void {
+        std::scoped_lock guard(this->lock);
+        this->type_infos.erase(obj);
+    }
 
     auto MXDynamicTypeInfoManager::repr() const -> std::string {
-         return "MXDynamicTypeInfoManager";
-     }
-
-
-
+        return "MXDynamicTypeInfoManager";
+    }
 
 
 }
